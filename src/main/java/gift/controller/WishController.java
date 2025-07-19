4498b76334a860e87fb9887074f07cd64d56a55e
jnujh <jnu21jihoon@gmail.com>
2025-07-19 16:21:34 +0900

    docs: README.md 수정

--- src/main/java/gift/controller/WishController.java
+++ src/main/java/gift/controller/WishController.java
@@ -14,7 +14,6 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/wishes")
